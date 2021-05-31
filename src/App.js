5a348740b165288c35b34d1af84528b237ec3044
Kush Patel <kushpatel03@hotmail.com>
2021-05-31 14:51:52 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hello Insta</h1>
+      
+      <div className="app__header"> 
+      
+      </div>
+      {/*header*/}
+
+      {/*post*/}
+      {/*post*/}
+      {/*post*/}
+    </div>
+  );
+}
+
+export default App;
